bae4ed16b5dfc0a55929555293de6d8f0a98fc1c
matsu <75355367+matsuwork@users.noreply.github.com>
2021-01-18 22:17:29 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ loading.src = "img/loading-circle.gif"
 const modal = document.querySelector('.modal');
 const body = document.querySelector('body');
 
+const hello = document.createElement('p');
+
 /* 5で使用
 const array = [
     {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
@@ -31,32 +33,57 @@ function closeModal() {
     body.classList.remove("no-scroll");
 }
 
-function writeList(array, index) {
-    let li = document.createElement('li');
-    let a = document.createElement('a');
-    ul.appendChild(li);
+//フォームの値をPromiseで取得
+function getName() {
+    return new Promise((resolve, reject) => {
+        resolve(document.form.text.value);
+    });
+}
 
-    let to = array[index].to;
-    let img = array[index].img;
-    let alt = array[index].alt;
-    let text = array[index].text;
+function writeName(str) {
+    hello.innerHTML = `こんにちは、${str}さん。`;
+    ul.before(hello);
+}
 
-    a.href = to;
-    li.appendChild(a);
-    a.innerHTML = `<img src="${img}" alt="${alt}">${text}`;
+function writeLists(array) {
+    for (var i = 0 ; i < array.length ; i++) {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        ul.appendChild(li);
+
+        let to = array[i].to;
+        let img = array[i].img;
+        let alt = array[i].alt;
+        let text = array[i].text;
+
+        a.href = to;
+        li.appendChild(a);
+        a.innerHTML = `<img src="${img}" alt="${alt}">${text}`;
+    }
 }
 
 
-async function writeArray() {
+async function submitTry() {
+    try {
+        let resName = await getName();
+        if (resName == '' ) {
+            return false;
+        }
+        writeName(resName);
+    } catch (err) {
+        console.error(err);
+        ul.innerHTML = 'エラーが発生しました';
+    }
+
     ul.innerHTML = '';
     ul.appendChild(loading);
-    try {
-        const response = await fetch('https://jsondata.okiba.me/v1/json/do9gM210114032953');
-        const postsData = await response.json();
-        for (var i = 0 ; i < postsData.data.length ; i++) {
-            writeList(postsData.data,i);
-        }
 
+    try {
+        let response = await fetch('https://jsondata.okiba.me/v1/json/do9gM210114032953');
+        let resJson = await response.json();
+        let resName = await getName();
+        writeName(resName);
+        writeLists(resJson.data);
     } catch (err) {
         console.error(err);
         ul.innerHTML = 'エラーが発生しました';
@@ -68,9 +95,10 @@ async function writeArray() {
 const openBtn = document.getElementById("open-btn")
 openBtn.addEventListener('click', openModal);
 
-const reqBtn = document.getElementById("req-btn");
-reqBtn.addEventListener('click',  function() {
-    writeArray();
+const reqBtn = document.getElementById("req-form");
+reqBtn.addEventListener('submit',  function(e) {
+    e.preventDefault();
+    submitTry();
     closeModal();
 });
 
